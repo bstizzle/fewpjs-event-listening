a883fc4e84430ab18703e4611bda42e35ab4a69e
bstizzle <whereyouben@gmail.com>
2020-12-09 16:38:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-function addingEventListener() {
+function addingEventListener(event, callback) {
+    const input = document.getElementById('input');
+    input.addEventListener('click', function(event) {alert('I was clicked!');});
 }
